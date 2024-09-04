d1acc8cb1974637786680b34c997e6ec48ff666d
jina1612 <jenny12161234@gmail.com>
2024-09-04 15:50:36 +0900

    camera 조정

--- Assets/2_Scripts/CameraManager.cs
+++ Assets/2_Scripts/CameraManager.cs
@@ -6,9 +6,15 @@ public class CameraManager : MonoBehaviour
     
     public static CameraManager Instance;
 
+    [SerializeField] private SpriteRenderer bgSrdr;
+    float cameraWidth;
+
     public void Init()
     {
         Instance = this;
+        Camera camera = Camera.main;
+        float cameraHeigh = camera.orthographicSize * 2f;
+        cameraWidth = cameraHeigh * camera.aspect;
     }
 
     public void OnFollow(Vector2 targetPos)
@@ -21,6 +27,14 @@ public class CameraManager : MonoBehaviour
         while (0.1f < Vector3.Distance(transform.position, targetPos))
         {
             transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * DataBaseManager.Instance.follwSpeed);
+
+            float bgRightX = bgSrdr.transform.position.x + bgSrdr.size.x;
+            float cmaerRightX = Camera.main.transform.position.x + cameraWidth / 2;
+
+            if(bgRightX < cmaerRightX)
+            {
+                bgSrdr.size = new Vector2 (bgSrdr.size.x + cameraWidth, bgSrdr.size.y);
+            }
             yield return null;
         }
     }
